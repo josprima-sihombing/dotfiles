4892946f56b1ef9d68ec9c5f8e868cab88f70095
Josprima Sihombing <jsihombing.id@gmail.com>
2025-04-22 16:55:14 +0700

    show diagnostic error in floating window with <leader>e

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -194,6 +194,10 @@ vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right win
 vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
 vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
 
+vim.keymap.set("n", "<leader>e", function()
+	vim.diagnostic.open_float(nil, { focus = false, border = "rounded" })
+end, { desc = "Show diagnostics in floating window" })
+
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
